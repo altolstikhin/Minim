c5a059a2a71fa36b95f0992e6b61e7e9e3fbd8b7
technosf <github.10.technomation@xoxy.net>
2017-02-25 21:13:59 -0800

    Modularizing

---
+++ javasound/src/main/java/ddf/minim/impl/JSMinimModule.java
@@ -0,0 +1,16 @@
+package ddf.minim.impl;
+
+import com.google.inject.AbstractModule;
+
+public class JSMinimModule
+        extends AbstractModule
+{
+
+    @Override
+    protected void configure()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
